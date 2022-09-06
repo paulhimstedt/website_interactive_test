270746186e2668cf44cfa792e8671441625b6636
paulhimstedt <71269641+paulhimstedt@users.noreply.github.com>
2022-09-06 15:31:53 +0200

    v2
    major changes still wip tho

--- muzascorner_test_web_video_switch/index.js
+++ muzascorner_test_web_video_switch/index.js
@@ -1,30 +1,32 @@
 var home_button = document.getElementById("home_button");
 var contact_button = document.getElementById("contact_button");
-var discografie_button = document.getElementById("discografie_button");
+var disco_button = document.getElementById("disco_button");
 var about_button = document.getElementById("about_button");
+var vid_conatiner = document.getElementById("vid-container");
 var active_scene = "home"
+    //document.body.style.backgroundImage = "url('/src/images/home.png')";
 
 function home_button_set_background() {
     transition(active_scene, "home")
-    document.body.style.backgroundImage = "url('/src/images/home.png')";
+        //document.body.style.backgroundImage = "url('/src/images/home.png')";
     active_scene = "home"
 }
 
 function contact_buttonn_set_background() {
     transition(active_scene, "contact")
-    document.body.style.backgroundImage = "url('/src/images/contact.png')";
+        //document.body.style.backgroundImage = "url('/src/images/contact.png')";
     active_scene = "contact"
 }
 
-function discografie_button_set_background() {
-    transition(active_scene, "discografie")
-    document.body.style.backgroundImage = "url('/src/images/disco.png')";
-    active_scene = "diskografie"
+function disco_button_set_background() {
+    transition(active_scene, "disco")
+        //document.body.style.backgroundImage = "url('/src/images/disco.png')";
+    active_scene = "disco"
 }
 
 function about_buttonn_set_background() {
     transition(active_scene, "about")
-    document.body.style.backgroundImage = "url('/src/images/about.png')";
+        //document.body.style.backgroundImage = "url('/src/images/about.png')";
     active_scene = "contact"
 }
 
@@ -36,20 +38,31 @@ function setupVideo() {
     })
 }
 
+function updateBackground(newBG) {
+    newBG = String(newBG)
+        //document.body.style.backgroundImage = "url('src/images/" + newBG + ".png')";
+        //document.body.style.backgroundImage = "url('src/images/" + newBG + ".png')";
+    document.getElementById("vid-container").style.backgroundImage = "url('src/images/" + newBG + ".png')";
+    //document.getElementById("myImage").style.backgroundImage = "url('src/images/" + newBG + ".png')";
+}
+
 function transition(current_scene, target_scene) {
     console.log(current_scene, " & ", target_scene)
-        //setupVideo()
-    const video = document.createElement('video');
+    setupVideo()
+    var video = document.createElement('video');
     switch (current_scene) {
         case "home":
             switch (target_scene) {
                 case "contact":
                     video.src = 'src/transitions/home_contact.mp4';
+                    updateBackground(target_scene);
                     break;
                 case "about":
                     video.src = 'src/transitions/home_about.mp4';
+                    updateBackground(target_scene);
                     break;
-                case "diskografie":
+                case "disco":
+                    updateBackground(target_scene);
                     video.src = 'src/transitions/home_disco.mp4';
                     break;
             }
@@ -58,12 +71,15 @@ function transition(current_scene, target_scene) {
             switch (target_scene) {
                 case "home":
                     video.src = 'src/transitions/contact_home.mp4';
+                    updateBackground(target_scene);
                     break;
                 case "about":
                     video.src = 'src/transitions/contact_about.mp4';
+                    updateBackground(target_scene);
                     break;
-                case "diskografie":
+                case "disco":
                     video.src = 'src/transitions/contact_disco.mp4';
+                    updateBackground(target_scene);
                     break;
             }
             break;
@@ -71,40 +87,73 @@ function transition(current_scene, target_scene) {
             switch (target_scene) {
                 case "home":
                     video.src = 'src/transitions/about_home.mp4';
+                    updateBackground(target_scene);
                     break;
                 case "contact":
                     video.src = 'src/transitions/about_contact.mp4';
+                    updateBackground(target_scene);
                     break;
-                case "diskografie":
+                case "disco":
                     video.src = 'src/transitions/about_disco.mp4';
+                    updateBackground(target_scene);
                     break;
             }
             break;
-        case "diskografie":
+        case "disco":
             switch (target_scene) {
                 case "home":
                     video.src = 'src/transitions/disco_home.mp4';
+                    updateBackground(target_scene);
                     break;
 
                 case "contact":
                     video.src = 'src/transitions/disco_contact.mp4';
+                    updateBackground(target_scene);
                     break;
 
                 case "about":
                     video.src = 'src/transitions/disco_about.mp4';
+                    updateBackground(target_scene);
                     break;
             }
             break;
 
 
     }
+    const container = document.getElementById('vid-container');
 
+
+
+
+
+    video.addEventListener('ended', function() {
+            container.removeChild(video);
+            console.log("removed image ");
+        })
+        /*sniff for existing vids*/
+    let numb = document.getElementById("vid-container").childElementCount;
+    let childs = document.getElementById("vid-container").childNodes
+    console.log(numb)
+        /*
+        if (numb != 1) {
+            for (let index = 1; index < numb; index++) {
+
+                console.log(childs[index]);
+                container.remove(video)
+                    //replaceChildren
+            }
+        }*/
     video.autoplay = true;
     video.controls = false;
     video.muted = true;
-    const container = document.getElementById('vid-container');
-    video.addEventListener('ended', function() {
-        video.style.display = "none";
-    })
+    video.style.height = "100%";
+    video.style.width = "100%";
+    video.style.objectFit = "cover";
+    video.style.objectPosition = "center center";
+    video.style.top = "0";
+    video.style.left = "0";
+    video.style.right = "0";
+    video.style.bottom = "0";
+
     container.appendChild(video);
 }
